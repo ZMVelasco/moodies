ef47a05029ad42b08678dbf3d0ab99a0ac080a8d
“Mariana <zaira.vl21@gmail.com>
2023-08-14 23:49:35 -0600

    Install Material UI

---
+++ src/routes/Home.jsx
@@ -0,0 +1,12 @@
+import Chip from '@mui/material/Chip';
+
+const Home = () => {
+  return (
+    <>
+        <div>Home</div>
+        <Chip label="Chip Filled" />
+    </>
+  )
+}
+
+export default Home
