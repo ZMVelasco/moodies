9073b6089b94d13eebc7ef41c3052a503e5d33f6
“Mariana <zaira.vl21@gmail.com>
2023-08-16 19:25:26 -0600

    Delete Home form, move it to Login

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,76 +1,10 @@
-import { Box, Button, TextField } from "@mui/material";
-import { useState } from "react";
 import Login from '../components/Login';
 
 const Register = () => {
-    const [email, setEmail] = useState("");
-    const [error, setError] = useState({
-        error: false,
-        message: "",
-    });
-
-    const emailValidation = (email) => {
-        // expresion regular para validar email
-        const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-        return regex.test(email);
-    };
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (!emailValidation(email)) {
-            setError({
-                error: true,
-                message: "El email no es valido",
-            });
-            return;
-        }
-        console.log(email);
-        setError({
-            error: false,
-            message: "",
-        });
-    };
 
     return (
         <>
             <Login />
-            <Box
-                component="form"
-                onSubmit={onSubmit}
-                autoComplete="off"
-            >
-                <TextField
-                    label="Email"
-                    variant="outlined"
-                    id="email"
-                    type="email"
-                    fullWidth
-                    required
-                    error={error.error}
-                    helperText={error.message}
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                />
-                <TextField
-                    label="Password"
-                    variant="outlined"
-                    id="password"
-                    type="password"
-                    fullWidth
-                    required
-                    error={error.error}
-                    helperText={error.message}
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                />
-                <Button
-                    variant="outlined"
-                    type="submit"
-                    sx={{ mt: 2 }}
-                >
-                    Submit
-                </Button>
-            </Box>
         </>
     );
 }
