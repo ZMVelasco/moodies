b739261211b1ae2f351514c23a06a7c418eea027
“Mariana <zaira.vl21@gmail.com>
2023-08-17 17:11:25 -0600

    Add dashboard redirect with otp

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-import { Route, Routes } from 'react-router-dom'
-// import { supabase } from './supabase/supabaseClient.js'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import { supabase } from './supabase/supabaseClient.js'
 import Home from './routes/Home.jsx'
 import Signup from './routes/Signup.jsx'
 import Dashboard from './routes/Dashboard.jsx'
+import LinkConfirmationPage from './routes/LinkConfirmation.jsx'
+import NotFound from './routes/NotFound.jsx'
 
-function App () {
+function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (!session) {
+        console.log('No session, redirecting to /');
+        navigate('/');
+      } else {
+        console.log('Session found, redirecting to /dashboard');
+        navigate('/dashboard');
+      }
+    });
+  }, [navigate]);
+
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/await-confirmation' element={<LinkConfirmationPage />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/routes/LinkConfirmation.jsx
@@ -0,0 +1,15 @@
+import { Container, Typography } from '@mui/material';
+
+
+const LinkConfirmationPage = () => {
+
+    return (
+        <Container maxWidth="sm">
+            <Typography variant="h5" align="center" mt={5}>
+                Confirming your magic link...
+            </Typography>
+        </Container>
+    );
+};
+
+export default LinkConfirmationPage;
