37ed0ccfac0ef92766a4ed17d72b937fc80e7612
“Mariana <zaira.vl21@gmail.com>
2023-08-16 22:35:32 -0600

    Add magic link log in

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 // import { supabase } from './supabase/supabaseClient.js'
 import Home from './routes/Home.jsx'
 import Signup from './routes/Signup.jsx'
+import Dashboard from './routes/Dashboard.jsx'
 
 function App () {
   return (
@@ -9,6 +10,7 @@ function App () {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </div>
   )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Container, Box, TextField, Button, Typography } from "@mui/material";
 import { supabase } from '../supabase/supabaseClient';
-import googleIcon from '../assets/google-icon.svg';
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -30,6 +29,28 @@ const Login = () => {
         }
     };
 
+    const handleMagicLinkLogin = async () => {
+        setError('');
+
+        try {
+            const { error } = await supabase.auth.signInWithOtp({
+                email: email,
+                options: {
+                    emailRedirectTo: 'https://example.com/welcome', // Replace with your redirect URL
+                },
+            });
+
+            if (error) {
+                setError(error.message);
+            } else {
+                console.log('Magic link sent successfully');
+                // You can provide user feedback here, e.g., show a message
+            }
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
     return (
         <Container maxWidth="sm">
             <Box mt={5}>
@@ -51,8 +72,12 @@ const Login = () => {
                 <Button variant="contained" color="primary" onClick={handleLogin}>
                     Login
                 </Button>
-                <Button variant="contained" startIcon={<img src={googleIcon} alt="Google Icon" />} onClick={() => supabase.auth.signIn({ provider: 'google' })}>
-                    Log in with Google
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={handleMagicLinkLogin}
+                >
+                    Log in with Magic Link
                 </Button>
                 {error && (
                     <Typography color="error" variant="body1">
