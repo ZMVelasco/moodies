7e898d43ec2ee1915dbfc0fba95b9bac2a88c93f
“Mariana <zaira.vl21@gmail.com>
2023-08-18 18:59:18 -0600

    Add MoodSelect component

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,9 +1,11 @@
+import MoodSelector from "../components/MoodSelect"
 import LogoutButton from "../components/LogOutButton"
 
 const Dashboard = () => {
     return (
         <>
             <div>Dashboard</div>
+            <MoodSelector />
             <LogoutButton />
         </>
     )
